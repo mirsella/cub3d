6e71d5d3a8419b46aa9f3c65b65a15e8f55e1aec
David LY <dly@e2r9p20.clusters.42paris.fr>
2023-03-02 19:14:08 +0100

    keyhook added

--- includes/struct.h
+++ includes/struct.h
@@ -6,17 +6,63 @@
 /*   By: dly <dly@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 12:12:05 by dly               #+#    #+#             */
-/*   Updated: 2023/03/02 14:38:32 by dly              ###   ########.fr       */
+/*   Updated: 2023/03/02 18:44:56 by dly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_H
 # define STRUCT_H
 
+typedef struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_img;
+
+typedef struct	s_ray
+{
+	double		posx; //position x du joueur
+	double		posy; //position y du joueur
+	double		dirx; //vecteur de direction (commence à -1 pour N, 1 pour S, 0 sinon)
+	double		diry; //vecteur de direction (commence à -1 pour W, 1 pour E, 0 sinon)
+	double		planx; //vecteur du plan (commence à 0.66 pour E, -0.66 pour W, 0 sinon)
+	double		plany; //vecteur du plan (commence à 0.66 pour N, -0.66 pour S, 0 sinon)
+	double		raydirx; //calcul de direction x du rayon
+	double		raydiry; //calcul de direction y du rayon
+	double		camerax; //point x sur la plan camera : Gauche ecran = -1, milieu = 0, droite = 1
+	int			mapx; // coordonée x du carré dans lequel est pos
+	int			mapy; // coordonnée y du carré dans lequel est pos
+	double		sidedistx; //distance que le rayon parcours jusqu'au premier point d'intersection vertical (=un coté x)
+	double		sidedisty; //distance que le rayon parcours jusqu'au premier point d'intersection horizontal (= un coté y)
+	double		deltadistx; //distance que rayon parcours entre chaque point d'intersection vertical
+	double		deltadisty; //distance que le rayon parcours entre chaque point d'intersection horizontal
+	int		stepx; // -1 si doit sauter un carre dans direction x negative, 1 dans la direction x positive
+	int		stepy; // -1 si doit sauter un carre dans la direction y negative, 1 dans la direction y positive
+	int		hit; // 1 si un mur a ete touche, 0 sinon
+	int		side; // 0 si c'est un cote x qui est touche (vertical), 1 si un cote y (horizontal)
+	double		perpwalldist; // distance du joueur au mur
+	int		lineheight; //hauteur de la ligne a dessiner
+	int		drawstart; //position de debut ou il faut dessiner
+	int		drawend; //position de fin ou il faut dessiner
+	int		x; //permet de parcourir tous les rayons
+}	t_ray;
+
 typedef struct s_map
 {
 	void	*mlx;
 	void	*win;
+	char	**map_matrix;
+	t_img	*img;	
 }	t_map;
+
+typedef struct s_data
+{
+	t_map	*map;
+	t_img	img;
+	t_ray	ray;
+}	t_data;
 
 #endif
---
+++ srcs/ft_put_pixel.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_put_pixel.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dly <dly@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/02 17:49:00 by dly               #+#    #+#             */
+/*   Updated: 2023/03/02 17:53:05 by dly              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+void	my_mlx_pixel_put(t_img *m, int y, int x, int color)
+{
+	char	*dst;
+
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
